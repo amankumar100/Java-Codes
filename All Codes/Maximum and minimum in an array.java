adcd8ec6ea7a58efaf3b89d0e59d8119e86d1f57
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-08-17 08:47:15 +0530

    Create Maximum and minimum in an array.java

---
+++ All Codes/Maximum and minimum in an array.java
@@ -0,0 +1,27 @@
+// Question: Write a Java program to find the maximum and minimum element in an array.
+
+import java.util.*;
+
+public class MaxMinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0], min = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+        sc.close();
+    }
+}
