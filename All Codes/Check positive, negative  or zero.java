8c140ae9e4b9e74b6b8ca95405410f8ce03e3dba
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-08-18 10:52:14 +0530

    Create Check positive, negative  or zero.java

---
+++ All Codes/Check positive, negative  or zero.java
@@ -0,0 +1,20 @@
+// Question: Write a Java program to check whether a number is positive, negative, or zero.
+
+import java.util.*;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        sc.close();
+    }
+}
