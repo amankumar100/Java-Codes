494d333959d6ff50471a7c388c85fa621ce29be9
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-08-20 10:12:25 +0530

    Create Character frequency in a string.java

---
+++ All Codes/Character frequency in a string.java
@@ -0,0 +1,22 @@
+// Question: Write a Java program to count the frequency of characters in a string.
+
+import java.util.*;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.println("Character frequencies:");
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+        sc.close();
+    }
+}
