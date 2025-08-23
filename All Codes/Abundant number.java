d5c9aa0c4fa58734a84f17e83e4cc31f3e55f0c6
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-08-23 13:09:49 +0530

    Create Abundant number.java

---
+++ All Codes/Abundant number.java
@@ -0,0 +1,23 @@
+// Question: Write a Java program to check if a number is an Abundant number.
+
+import java.util.*;
+
+public class AbundantNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) sum += i;
+        }
+
+        if (sum > num) {
+            System.out.println(num + " is an Abundant number.");
+        } else {
+            System.out.println(num + " is not an Abundant number.");
+        }
+        sc.close();
+    }
+}
