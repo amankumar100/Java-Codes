669f4c0dcd09f6d226795a5abf6bf6c8c62f87ab
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-08-28 14:41:50 +0530

    Create Selection sort.java

---
+++ All Codes/Selection sort.java
@@ -0,0 +1,34 @@
+// Question: Write a Java program to implement Selection Sort.
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        selectionSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        sc.close();
+    }
+}
