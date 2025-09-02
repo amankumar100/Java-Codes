9935f37e55f6db0d2a102fe17c429fd3aff34d9f
Aman Kumar <kr.aman8025@gmail.com>
2025-09-02 11:27:36 +0530

    Create squarehollowpattern.java

---
+++ Codes/Patterns/squarehollowpattern.java
@@ -0,0 +1,29 @@
+/* Write a java program to print Square hollow pattern.
+
+******
+*    *
+*    *
+*    *
+*    *
+******  
+
+*/
+
+import java.util.*;
+
+public class squarehollowpattern {
+    public static void main(String args[]) {
+        int i, j;
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j < 6; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
