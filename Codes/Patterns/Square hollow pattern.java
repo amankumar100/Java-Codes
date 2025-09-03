1d3033fdb5229643e370be888f4b486f92a5fef9
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-09-03 11:00:18 +0530

    Update Square hollow pattern.java

--- Codes/Patterns/Square hollow pattern.java
+++ Codes/Patterns/Square hollow pattern.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 public class squarehollowpattern {
     public static void main(String args[]) {
-        int i, j;
+        int i, j, n = 6;
         for (i = 0; i < 6; i++) {
             for (j = 0; j < 6; j++) {
                 if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
