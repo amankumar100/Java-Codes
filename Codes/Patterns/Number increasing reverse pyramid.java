5d3e110f7f68ee343ce6ac355bc82bfb773c5254
Aman Kumar <121876356+amankumar100@users.noreply.github.com>
2025-09-04 10:43:44 +0530

    Create Number increasing reverse pyramid.java

---
+++ Codes/Patterns/Number increasing reverse pyramid.java
@@ -0,0 +1,25 @@
+/* Java Program to print Number-increasing reverse pyramid pattern
+
+1 2 3 4 5 6 
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+*/
+
+import java.util.*;
+
+public class numberincreasingreveresepyramid {
+    public static void main(String args[]){
+    {
+        int i, j, n = 6;
+        for (i = n; i >= 1; i--) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
